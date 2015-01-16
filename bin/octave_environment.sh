7c313a7d8d93f2dc925b8f64466831568a8660e9
Will Clark <wclark3@gmail.com>
2015-01-16 13:48:07 -0600

    using vendor again

--- bin/octave_environment.sh
+++ bin/octave_environment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATH=$PATH:/app/octave/bin
+export PATH=$PATH:/app/vendor/octave/bin
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/gcc/lib64:/app/octave/deps/qrupdate/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/vendor/gcc/lib64:/app/vendor/octave/deps/qrupdate/lib
