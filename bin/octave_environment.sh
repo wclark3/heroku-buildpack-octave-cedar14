4bf7fbf7672fe4da3fab57be33ea2a874e9352ec
Will Clark <wclark3@gmail.com>
2015-01-16 14:04:31 -0600

    use secret .heroku folder

--- bin/octave_environment.sh
+++ bin/octave_environment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATH=$PATH:/app/vendor/octave/bin
+export PATH=$PATH:/app/.heroku/octave/bin
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/vendor/gcc/lib64:/app/vendor/octave/deps/qrupdate/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/.heroku/gcc/lib64:/app/.heroku/octave/deps/qrupdate/lib
